45e63738b55644d327247d2c96d2f504f06ea8fd
Ulysse DUPONT-LATAPIE <ulysseh2o@free.fr>
2022-03-27 16:53:26 +0200

    added temporary game info storage for lobby tests

---
+++ client/model/ClientData.java
@@ -0,0 +1,16 @@
+package model;
+
+public class ClientData {
+    // THIS IS A TEMPORARY CLASS AND HAS NO INTENT OF BEING DEFINITIVE
+
+    private int clientID;
+    private int tcpPort;
+
+    public int getClientID() {
+        return clientID;
+    }
+
+    public int getTCPPort() {
+        return tcpPort;
+    }
+}
