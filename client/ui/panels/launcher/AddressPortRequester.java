a031837b132d84a0de809d9ba18f909a105ee42c
Ulysse DUPONT-LATAPIE <ulysseh2o@free.fr>
2022-03-31 17:34:21 +0200

    added player pseudo support

--- client/ui/panels/launcher/AddressPortRequester.java
+++ client/ui/panels/launcher/AddressPortRequester.java
@@ -16,7 +16,8 @@ public class AddressPortRequester extends JPanel {
 
     private class IPInputBox extends JTextField {
         private IPInputBox() {
-            setText("Enter IP address here");
+            // TODO : allow "localhost" string / device address
+            setText("127.0.0.1");
             setFont(new Font("arial", 1, 50)); // name, style, size. Style defines if it's bold, italic, etc.
             setEditable(true);
             setVisible(true);
@@ -60,8 +61,10 @@ public class AddressPortRequester extends JPanel {
 
     private class PortInputBox extends JTextField {
 
+        private static final int DEFAULT_PORT = 4242;
+
         private PortInputBox() {
-            setText("Enter port here");
+            setText(Integer.toString(DEFAULT_PORT));
             setFont(new Font("arial", 1, 50)); // name, style, size. Style defines if it's bold, italic, etc.
             setEditable(true);
             setVisible(true);
@@ -107,12 +110,52 @@ public class AddressPortRequester extends JPanel {
         }
     }
 
+    private class PseudoInputBox extends JTextField {
+
+        private PseudoInputBox() {
+            setText("no name");
+            setFont(new Font("arial", 1, 50)); // name, style, size. Style defines if it's bold, italic, etc.
+            setEditable(true);
+            setVisible(true);
+            getDocument().addDocumentListener(new DocumentListener() {
+
+                @Override
+                public void changedUpdate(DocumentEvent e) {
+                    updateConnectionValidity();
+                }
+
+                @Override
+                public void insertUpdate(DocumentEvent e) {
+                    updateConnectionValidity();
+                }
+
+                @Override
+                public void removeUpdate(DocumentEvent e) {
+                    updateConnectionValidity();
+                }
+                
+            });
+        }
+
+        public boolean checkPseudoFormat() {
+            int l = getText().length();
+            boolean answer = 0 < l && l <= 8;
+            if (answer) {
+                setForeground(Color.GREEN);
+            }
+            else {
+                setForeground(Color.RED);
+            }
+            return answer;
+        }
+    }
+
     private class ConnectionButton extends JButton {
 
         private ConnectionButton() {
             super();
             setText("Connect");
-            setEnabled(false);
+            setEnabled(true);
             addActionListener(new ActionListener() {
 
                 @Override
@@ -127,6 +170,7 @@ public class AddressPortRequester extends JPanel {
 
     private IPInputBox inputBox;
     private PortInputBox portInputBox;
+    private PseudoInputBox pseudoInputBox;
     private ConnectionButton connectionButton;
     
     public AddressPortRequester() {
@@ -134,14 +178,16 @@ public class AddressPortRequester extends JPanel {
         // default layout : flow layout
         inputBox = new IPInputBox();
         portInputBox = new PortInputBox();
+        pseudoInputBox = new PseudoInputBox();
         connectionButton = new ConnectionButton();
         add(inputBox);
         add(portInputBox);
+        add(pseudoInputBox);
         add(connectionButton);
     }
 
     private void updateConnectionValidity() {
-        boolean canConnect = inputBox.checkIPFormat() && portInputBox.checkPortFormat();
+        boolean canConnect = inputBox.checkIPFormat() && portInputBox.checkPortFormat() && pseudoInputBox.checkPseudoFormat();
         // Due to java being a lazy language, the second test is only run when the first one is passed,
         // leading to the port changing color only when the address is valid. This is not a bug.
         // However, it is possible to change this behavior if wanted.
