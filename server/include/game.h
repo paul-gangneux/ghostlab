eb045baf5e14b7ac384cccac3fd60ee544fcd267
Paul Gangneux <paul.v.gangneux@gmail.com>
2022-03-31 17:27:19 +0200

    petites modifs et restructuration

--- server/include/game.h
+++ server/include/game.h
@@ -1,4 +1,9 @@
 #ifndef GAME_H
 #define GAME_H
 
+#include <sys/types.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
 #endif
--- server/src/game.c
+++ server/src/game.c
@@ -1,1 +1,15 @@
-//TODO
+#include "game.h"
+//TODO: changer les types
+
+typedef struct game {
+  u_int8_t id;
+  u_int16_t w,h;
+  u_int8_t nbOfPlayers;
+  u_int8_t nbOfGhosts;
+  char multicast_port[4];
+
+  int* labyrinth;
+} game;
+
+// ce sera sûrement la même tout le temps, c'est donc une variable partagée
+const char multicast_ip[15];
