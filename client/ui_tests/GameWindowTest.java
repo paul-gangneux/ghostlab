bfe95de44c064bf708abc40d637320852b9d7e79
Ulysse DUPONT-LATAPIE <ulysseh2o@free.fr>
2022-04-03 04:32:51 +0200

    added main game window support and tests

---
+++ client/ui_tests/GameWindowTest.java
@@ -0,0 +1,18 @@
+package ui_tests;
+
+import model.GameInfo;
+import model.PlayerModel;
+import ui.GameWindow;
+
+public class GameWindowTest {
+    public static void main(String [] args)  {
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                GameInfo gi = new GameInfo(1, 4, 10, 5); // game #1, 4 players, laby is 10x5.
+                PlayerModel pm = new PlayerModel(2, 2); // player is set to be at (2, 2)
+                GameWindow gw = new GameWindow(gi, pm);
+                gw.setVisible(true);
+            }
+        });
+    }
+}
