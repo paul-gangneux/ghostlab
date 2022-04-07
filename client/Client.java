d6b2f0a00ecf6693204005adc457a35716af672a
yanoux <yaniakli9@gmail.com>
2022-04-07 22:13:53 +0200

    client fonctionnel

--- client/Client.java
+++ client/Client.java
@@ -1,33 +1,67 @@
 import java.io.*;
 import java.net.*;
-public class Client {
-  static final int port = 8080; 
-  /*TODO : verifier si la connection au socket s'est bien effectué 
-  et si oui entrer un pseudo.
-  Ensuite le serveur doit generer un id et l'attribuer au joueur en question.
-  */
-  public static void main(String[] args){
-    try{
-      //TCP pour les requetes etc..
-      Socket socket = new Socket("localhost",port);
-      
-      BufferedReader readBuff = new BufferedReader(
-        new InputStreamReader((socket.getInputStream())));
-      
-        PrintWriter writeBuff = new PrintWriter(
-        new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())));
-      //UDP servira uniquement pour les messages privées entre joueurs
-      String s = "content*";
-      DatagramSocket dso=new DatagramSocket();
-      byte[]data;
-        data=s.getBytes();
-        DatagramPacket paquet=new
-        DatagramPacket(
-          data,data.length,InetAddress.getByName("localhost"),port);
-        dso.send(paquet);
+import java.util.Scanner;
+
+public class Client extends Thread {
+    private Socket server ;
+    private int portUdp ;
+    private byte[] buffer ;
+    private DatagramSocket datagramSocket ;
+    private InetAddress inetAddress ;
+    private Scanner key; // Scanner for input
+
+    public Client(String ip, int portTcp,int portUdp){
+        try {
+            server = new Socket(ip, portTcp);
+            datagramSocket = new DatagramSocket();
+            inetAddress = InetAddress.getByName(ip);
+            this.portUdp = portUdp;
+            key = new Scanner(System.in);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-    catch(Exception e){
-      e.printStackTrace();
+
+    public boolean checkRequest(String request){
+        if(request.isEmpty() || request.equals("")){
+          System.out.println("ERROR : empty request");
+          return false;
+        }
+        if(!(request.endsWith("***"))){
+          System.out.println("ERROR : bad fomulated request");
+          return false;
+        }
+        return true;
     }
-  }
+    
+    @Override
+    public void run(){
+        DataInputStream istream = null;
+        DataOutputStream ostream = null;
+        DatagramPacket datagramPacket= null;
+        try {
+            while(true){
+                key = new Scanner(System.in);
+                istream = new DataInputStream(server.getInputStream()); 
+                ostream = new DataOutputStream(server.getOutputStream());
+                
+                System.out.println(istream.readUTF());  // Print what the server sends
+                System.out.print(">");
+                String tosend = key.nextLine();
+                if(checkRequest(tosend)){
+                    ostream.writeUTF(tosend);   // Send whatever the user typed to the server
+                    System.out.println(istream.readUTF());  // read what the server sends before exiting.
+                }
+            }
+            //TODO: when receive the messages through UDP 
+        }
+         catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String argv[]) {
+        Client thr1 = new Client("localhost",4999,5555);
+        thr1.run();
+    }
 }
