8619bb9d6c008205ad63bdd2965a15047c3ec5f3
Ulysse DUPONT-LATAPIE <ulysseh2o@free.fr>
2022-04-23 03:20:51 +0200

    View class first draft and window swap tests

--- client/src/ui/View.java
+++ client/src/ui/View.java
@@ -1,5 +1,23 @@
 package ui;
 
+import javax.swing.JFrame;
+
+import client.Client;
+
 public class View {
-    
+    private Client c;
+
+    private JFrame currentWindow;
+
+    public View(Client c) {
+        this.c = c;
+    }
+
+    public void switchToWindow(JFrame window) {
+        if (currentWindow != null) { // First window of the UI
+            currentWindow.dispose();
+        }
+        currentWindow = window;
+        currentWindow.setVisible(true);
+    }
 }
