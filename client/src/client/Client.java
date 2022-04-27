dbdc9d7e3316f0eabcf6aefadab6b59753ee0521
Ulysse DUPONT-LATAPIE <ulysseh2o@free.fr>
2022-04-27 04:47:05 +0200

    auxiliary methods in client, for testing

--- client/src/client/Client.java
+++ client/src/client/Client.java
@@ -1,6 +1,42 @@
 package client;
 
+import java.util.ArrayList;
+
+import model.MessageInfo;
+import ui.GameWindow;
+import ui.View;
+
 public class Client {
+
+    private View view; // good way to do
+
+    private GameWindow gw; // bad way to do (for tests)
+
+    public void setGameWindow(GameWindow gw) {
+        this.gw = gw;
+    }
+
+    public ArrayList<String> getAllOtherPlayersNames() {
+        ArrayList<String> res = new ArrayList<String>();
+        // TODO : this is used for tests, please make it real !
+        for (int i = 1; i < 10; i++) {
+            res.add("Player " + Integer.toString(i));
+        }
+        return res;
+    }
+
+    public String getName() {
+        return "Uly";
+    }
+
+    public boolean hasTeam() {
+        return true; // TODO
+    }
+
+    public void sendOnChat(MessageInfo mi) {
+        gw.addMessage(mi);
+    }
+
     static ClientTcp c1;
     static ClientUdp c2;
     public static void main(String[] args) {
