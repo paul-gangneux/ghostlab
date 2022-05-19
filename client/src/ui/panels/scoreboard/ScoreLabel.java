ff5e60e68b3175395ad7bd18012b81b6130421c6
yanoux <yaniakli9@gmail.com>
2022-05-19 18:47:52 +0200

    score and background added

--- client/src/ui/panels/scoreboard/ScoreLabel.java
+++ client/src/ui/panels/scoreboard/ScoreLabel.java
@@ -11,6 +11,6 @@ public class ScoreLabel extends JLabel {
     public ScoreLabel(PlayerModel pm) {
         setForeground(Color.RED);
         setFont(getFont().deriveFont(Font.BOLD));
-        setText(pm.getPseudo() + " : " + Integer.toString(pm.getScore()));
+        setText(pm.getPseudo() + " :       " + Integer.toString(pm.getScore())+"     ");
     }
 }
